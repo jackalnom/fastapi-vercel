9dabea236782ff5f47ee369a4f06a646b76f3322
jackalnom <jackalnom@gmail.com>
2023-08-02 11:28:17 -0700

    finish checking in

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ app = FastAPI()
 def files_open():
     fd_directory = "/proc/self/fd"
     fds = os.listdir(fd_directory)
-    fd_info = {fd: os.readlink(os.path.join(fd_directory, fd)) for fd in fds}
+    fd_info = {}
+    for fd in fds:
+        try:
+            fd_info[fd] = os.readlink(os.path.join(fd_directory, fd))
+        except FileNotFoundError:
+            fd_info[fd] = "File descriptor closed before it could be read"
     return fd_info
